2b0bf183cb63a1e31347d178dec5881f27a02424
Jumpei Ogawa <phanective@gmail.com>
2015-05-04 18:25:15 +0900

    Add Array.prototype.includes

--- polyfills.js
+++ polyfills.js
@@ -28,4 +28,38 @@
 			return String.prototype.indexOf.apply(this, arguments) !== -1;
 		};
 	}
+
+	/*
+	 * Array.prototype.includes
+	 * Brought from MDN: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes
+	 * License: CC0 1.0 Universal (Public Domain) http://creativecommons.org/publicdomain/zero/1.0/
+	 */
+	if (![].includes) {
+		Array.prototype.includes = function(searchElement /*, fromIndex*/ ) {
+			var O = Object(this);
+			var len = parseInt(O.length) || 0;
+			if (len === 0) {
+				return false;
+			}
+			var n = parseInt(arguments[1]) || 0;
+			var k;
+			if (n >= 0) {
+				k = n;
+			} else {
+				k = len + n;
+				if (k < 0) {k = 0;}
+			}
+			var currentElement;
+			while (k < len) {
+				currentElement = O[k];
+				if (searchElement === currentElement ||
+					(searchElement !== searchElement && currentElement !== currentElement))
+				{
+					return true;
+				}
+				k++;
+			}
+			return false;
+		};
+	}
 }());
