008ecfef7161271709aa724ced2c8cc5895b203a
Jumpei Ogawa <ogawa@growaspeople.org>
2015-06-18 19:38:30 +0900

    Check existence of window object before declaring saveAs

--- polyfills.js
+++ polyfills.js
@@ -72,40 +72,42 @@
 	 * @copyright MIT, BSD. Free to clone, modify and distribute for commercial and personal use.
 	 * @see https://gist.github.com/MrSwitch/3552985, https://gist.github.com/phanect/46b692241c6bbe456994
 	 */
-	window.saveAs || ( window.saveAs = (window.navigator.msSaveBlob ? function(b,n){ return window.navigator.msSaveBlob(b,n); } : false) || window.webkitSaveAs || window.mozSaveAs || window.msSaveAs || (function(){
-		// URL's
-		window.URL || (window.URL = window.webkitURL);
+	if (window) {
+		window.saveAs || ( window.saveAs = (window.navigator.msSaveBlob ? function(b,n){ return window.navigator.msSaveBlob(b,n); } : false) || window.webkitSaveAs || window.mozSaveAs || window.msSaveAs || (function(){
+			// URL's
+			window.URL || (window.URL = window.webkitURL);
 
-		if(!window.URL){
-			return false;
-		}
-
-		return function(blob,name){
-			var url = URL.createObjectURL(blob);
-
-			// Test for download link support
-			if( "download" in document.createElement("a") ){
-
-				var a = document.createElement("a");
-				a.setAttribute("href", url);
-				a.setAttribute("download", name);
-
-				// Create Click event
-				var clickEvent = document.createEvent ("MouseEvent");
-				clickEvent.initMouseEvent ("click", true, true, window, 0,
-										   clickEvent.screenX, clickEvent.screenY, clickEvent.clientX, clickEvent.clientY,
-										   clickEvent.ctrlKey, clickEvent.altKey, clickEvent.shiftKey, clickEvent.metaKey,
-										   0, null);
-
-				// dispatch click event to simulate download
-				a.dispatchEvent (clickEvent);
-
+			if(!window.URL){
+				return false;
 			}
-			else{
-				// fallover, open resource in new tab.
-				window.open(url, "_blank", "");
-			}
-		};
 
-	})() );
+			return function(blob,name){
+				var url = URL.createObjectURL(blob);
+
+				// Test for download link support
+				if( "download" in document.createElement("a") ){
+
+					var a = document.createElement("a");
+					a.setAttribute("href", url);
+					a.setAttribute("download", name);
+
+					// Create Click event
+					var clickEvent = document.createEvent ("MouseEvent");
+					clickEvent.initMouseEvent ("click", true, true, window, 0,
+											   clickEvent.screenX, clickEvent.screenY, clickEvent.clientX, clickEvent.clientY,
+											   clickEvent.ctrlKey, clickEvent.altKey, clickEvent.shiftKey, clickEvent.metaKey,
+											   0, null);
+
+					// dispatch click event to simulate download
+					a.dispatchEvent (clickEvent);
+
+				}
+				else{
+					// fallover, open resource in new tab.
+					window.open(url, "_blank", "");
+				}
+			};
+
+		})() );
+	}
 }());
